24ea63b678c0ea4bf43797c7d5e4be73f10a0ebd
kenmckaba <ken@mckaba.com>
2026-03-19 20:56:52 -0700

    formatting

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -4,12 +4,14 @@ const path = require('path')
 const nextConfig = {
   env: {
     NEXT_PUBLIC_GRAPHQL_URL:
-      process.env.NEXT_PUBLIC_GRAPHQL_URL || 'http://192.168.5.106:4000/graphql',
-    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL || 'ws://192.168.5.106:4000/graphql',
+      process.env.NEXT_PUBLIC_GRAPHQL_URL ||
+      'http://192.168.5.106:4000/graphql',
+    NEXT_PUBLIC_WS_URL:
+      process.env.NEXT_PUBLIC_WS_URL || 'ws://192.168.5.106:4000/graphql',
   },
   turbopack: {
-    root: process.cwd()?.includes('apps') 
-      ? path.resolve(process.cwd(), '../../') 
+    root: process.cwd()?.includes('apps')
+      ? path.resolve(process.cwd(), '../../')
       : process.cwd(),
   },
   // Output standalone for serverless deployment compatibility
